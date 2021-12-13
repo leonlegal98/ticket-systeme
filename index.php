245df137ab3109fe6ed8c2ddb63a04019023cec2
Léon LE GAL <leon@MacBook-Air-de-Leon.local>
2021-12-13 11:47:23 +0100

    first

---
+++ index.php
@@ -0,0 +1,44 @@
+<?php
+include 'functions.php';
+$pdo = pdo_connect_mysql();
+$stmt = $pdo->prepare('SELECT * FROM tickets ORDER BY created DESC');
+$stmt->execute();
+$tickets = $stmt->fetchAll(PDO::FETCH_ASSOC);
+
+
+?>
+
+<div class="content home">
+
+    <h2>Tickets</h2>
+
+    <p>liste des tickets.</p>
+
+    <div class="btns">
+        <a href="create.php" class="btn">Creer Ticket</a>
+    </div>
+
+    <div class="tickets-list">
+        <?php foreach ($tickets as $ticket) : ?>
+            <a href="view.php?id=<?= $ticket['id'] ?>" class="ticket">
+                <span class="con">
+                    <?php if ($ticket['status'] == 'open') : ?>
+                        <i class="far fa-clock fa-2x"></i>
+                    <?php elseif ($ticket['status'] == 'resolved') : ?>
+                        <i class="fas fa-check fa-2x"></i>
+                    <?php elseif ($ticket['status'] == 'closed') : ?>
+                        <i class="fas fa-times fa-2x"></i>
+                    <?php endif; ?>
+                </span>
+                <span class="con">
+                    <span class="title"><?= htmlspecialchars($ticket['title'], ENT_QUOTES) ?></span>
+                    <span class="msg"><?= htmlspecialchars($ticket['msg'], ENT_QUOTES) ?></span>
+                </span>
+                <span class="con created"><?= date('F dS, G:ia', strtotime($ticket['created'])) ?></span>
+            </a>
+        <?php endforeach; ?>
+    </div>
+
+</div>
+
+<?= template_footer() ?>
